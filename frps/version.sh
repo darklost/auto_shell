d4896c4ab8424eca14460ae4915a0a22e763d854
D.K <453523830@qq.com>
2020-07-07 09:41:34 +0800

    Update version.sh

--- frps/version.sh
+++ frps/version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export FRPS_VER=0.27.0 #frps对的版本
+export FRPS_VER=0.33.0 #frps对的版本
 export FRPS_INIT="https://raw.githubusercontent.com/darklost/auto_shell/master/frps/frps.init" #frps服务脚本位置
 export github_download_url="https://github.com/fatedier/frp/releases/download" #下载地址目录 https://github.com/fatedier/frp/releases/download/v0.27.0/frp_0.27.0_linux_amd64.tar.gz
